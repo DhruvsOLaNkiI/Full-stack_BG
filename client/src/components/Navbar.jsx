f957a77e0777750531d0ebc121ed464310dc8cb8
Dhruv <sdhruv640@gmail.com>
2025-11-25 00:19:26 +0530

    chore: change get started to register

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -60,7 +60,7 @@ const Navbar = () => {
                     ) : (
                         <>
                             <Link to="/login" className="nav-link">Login</Link>
-                            <Link to="/register" className="btn btn-primary">Get Started</Link>
+                            <Link to="/register" className="btn btn-primary">Register</Link>
                         </>
                     )}
 
