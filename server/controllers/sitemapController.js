38b87e83ab829a139f56c973bbda092dc08495a3
Dhruv <sdhruv640@gmail.com>
2025-11-27 00:27:31 +0530

    fix: update production URLs for sitemap and categeore

--- server/controllers/sitemapController.js
+++ server/controllers/sitemapController.js
@@ -5,7 +5,7 @@ exports.getSitemap = async (req, res) => {
         // Fetch all posts (you might want to limit this if you have thousands)
         const posts = await Post.findAll({});
 
-        const baseUrl = 'https://kirdarbarcelona.vercel.app';
+        const baseUrl = 'https://www.kirdarbarcelona.com';
 
         let xml = '<?xml version="1.0" encoding="UTF-8"?>';
         xml += '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">';
